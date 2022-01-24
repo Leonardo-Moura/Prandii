cecf76e3d1a06a1f91e2fa8760b278444a8e65b4
Leonardo Moura <leonardo.moura@maps.com.br>
2022-01-23 23:54:47 -0300

    Iniciando o projeto

---
+++ core/src/main/java/com/treinamento/prandii/PrandiiApplication.java
@@ -0,0 +1,13 @@
+package com.treinamento.prandii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrandiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrandiiApplication.class, args);
+	}
+
+}
---
+++ core/src/test/java/com/treinamento/prandii/PrandiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinamento.prandii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrandiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
